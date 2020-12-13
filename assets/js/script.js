c853eeb8e74ddd50d47b4d1c22529a7d9d2bbba3
Laura Micheller Peterson <michellepeterson.mishmish@gmail.com>
2020-12-12 17:24:59 -0700

    added nested api call to get all required data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,49 @@
-// weather api key = 2c1512576f41358397a22bf62cab49d6
-
-// - Example of API call:
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2c1512576f41358397a22bf62cab49d6
-
-// api.openweathermap.org/data/2.5/weather?q=Austin,us&appid=2c1512576f41358397a22bf62cab49d6
-
 function myFunction() {
-    var searchTerm = document.querySelector("#searchTerm").nodeValue;
+    var searchTerm = document.querySelector("#searchTerm").value;
     console.log("searching for: " + searchTerm);
 
     fetch(
-        // 'api.openweathermap.org/data/2.5/weather?q=' + searchTerm + ',us&appid=2c1512576f41358397a22bf62cab49d6'
-        'api.openweathermap.org/data/2.5/weather?q=Austin,us&appid=2c1512576f41358397a22bf62cab49d6'
+        'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm + ',us&appid=2c1512576f41358397a22bf62cab49d6'
         )
-    .then(function(response) {
-        return response.json();
+    .then(function(response1) {
+        return response1.json();
     })
-    .then(function(response) {
-        console.log(response.data[0]);
+    .then(function(response1) {
+        console.log(response1);
         var responseContainerEl = document.querySelector('#response-container');
         responseContainerEl.innerHTML = '';
+
+        var cityNameEl = document.createElement('h2');
+        cityNameEl.textContent = response1.name;
+        responseContainerEl.appendChild(cityNameEl);
+
+        // add date
+
+        fetch(
+            'https://api.openweathermap.org/data/2.5/onecall?lat=' + response1.coord.lat + '&lon=' + response1.coord.lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=2c1512576f41358397a22bf62cab49d6'
+            )
+        .then(function(response2) {
+            return response2.json();
+        })
+        .then(function(response2) {
+            console.log(response2);
+        
+
+            var weatherTempEl = document.createElement('h2');
+            weatherTempEl.textContent = response2.current.temp;
+            responseContainerEl.appendChild(weatherTempEl);
+
+            var weatherHumidityEl = document.createElement('h2');
+            weatherHumidityEl.textContent = response2.current.humidity;
+            responseContainerEl.appendChild(weatherHumidityEl);
+
+            var windSpeedEl = document.createElement('h2');
+            windSpeedEl.textContent = response2.current.wind_speed;
+            responseContainerEl.appendChild(windSpeedEl);
+
+            var uvIndexEl = document.createElement('h2');
+            uvIndexEl.textContent = response2.current.uvi;
+            responseContainerEl.appendChild(uvIndexEl);
+        });
     });
 }
